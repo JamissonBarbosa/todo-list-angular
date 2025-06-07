9b3c183c1c228bbb091efcba1f58466b07a0282a
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2025-06-07 20:40:48 -0300

    feat: deploy project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, TodoCardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
